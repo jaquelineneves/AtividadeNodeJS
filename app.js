a19afa1a469deca6efb8f60e8db03e34a1c0f496
jaque <jaque.nevees2@gmail.com>
2019-07-09 08:20:25 -0300

    Programa de NodeJS

---
+++ app.js
@@ -0,0 +1,33 @@
+    const express = require('express');
+
+    const PORT = 3000;
+
+    const app = express();
+
+    const db = require('./config/database');
+    const bodyParser = require('body-parser');
+    const cors = require('cors');
+
+    app.use(cors({
+        origin: ['http://localhost:4200'],
+        methods: ["GET","POST","PUT","DELETE"],
+        allowedHeaders: ["Content-Type","Authorization"]
+    }));
+
+    app.use(bodyParser.json());
+
+    db.authenticate()
+        .then(() => console.log('Database connected...'))
+        .catch(err => console.log('Error: ' + err))
+
+        app.set("json spaces",4);
+
+        const index = require('./routes/index');
+        const clientes = require('./routes/clientes');
+    
+        app.use('/', index);
+        app.use('/clientes', clientes);
+
+        app.get("/", (req,res)=> res.json({status: "Nodejs backend"}));
+    
+        app.listen(PORT, () => console.log("escutando na porta "+PORT));
